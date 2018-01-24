20f62e9cc390cdb2dab1c948933a227b7654e3f0
Emily Nassi <emilynassi@Emilys-MacBook-Pro-2.local>
2018-01-24 15:05:36 -0500

    Edits

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,5 +1,5 @@
--- You need a list of all the actors who have Display the first and last names of all actors from the table actor.
-SELECT first_name, last_name FROM sakila.actor;
+-- You need a list of all the actors from the table actor.
+SELECT first_name, last_name FROM actor;
 
 --1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 SELECT UPPER(CONCAT(first_name, " ", last_name)) AS 'Actor Name'
